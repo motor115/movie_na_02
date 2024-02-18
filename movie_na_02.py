6c3864003a1c13797d228ca0c3c4df340f9db5e2
motor115 <motor115@gmail.com>
2024-02-19 00:51:20 +0900

    Update movie_na_02.py

--- movie_na_02.py
+++ movie_na_02.py
@@ -1,5 +1,17 @@
 import openai
 import streamlit as st
+from typing import Any
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+
+class JapaneseCharacterTextSplitter(RecursiveCharacterTextSplitter):
+    def __init__(self, **kwargs: Any):
+        separators = ["\n\n", "\n", "。", "、", " ", ""]
+        super().__init__(separators=separators, **kwargs)
+
+japanese_spliter = JapaneseCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=0,
+)
 openai.api_key = 'your-api-key-here'
 st.header('音声文字起こしアプリ')
 
@@ -12,12 +24,31 @@ if upload_file is not None:
     file_name=upload_file.name.split('.')[0]
     if upload_file.size > 25000000:
         st.error('エラー：ファイルが25MBを超えています。APIの上限により25MB以上のファイルは文字起こし不可です。ファイルを分割する等容量を少なくしてください', icon="🚨")
+    trans_start=st.button('文字起こし開始')
 
-if st.button('文字起こし開始',key="w_button"):
-    if upload_file is None:
-        st.error('エラー：ファイルが選択されていません', icon="🚨")
-    else:
-        with st.spinner('***音声文字起こしを実行中です...***'):
-            trans= openai.Audio.transcribe("whisper-1" ,upload_file)["text"]
-        st.success('***音声文字起こしを完了しました***')
-        st.write(trans)
+    if trans_start:
+        if upload_file is None:
+            st.error('エラー：ファイルが選択されていません', icon="🚨")
+        else:
+            with st.spinner('***音声文字起こしを実行中です...***'):
+                trans= openai.Audio.transcribe("whisper-1" ,upload_file)["text"]
+            st.success('***音声文字起こしを完了しました***')
+            st.write("***文字起こし結果***")
+            st.write(trans)
+            with st.spinner('***日本語の修正中です...***'):
+                texts = japanese_spliter.split_text(trans)
+                texts_modified=""
+                for text in texts:            
+                    prompt=f"##音声文字起こしで不自然な文を削除し、自然な文章に修正してください。\n##音声文字起こし\n{text}\n##修正した文章\n"
+                    messages=[{"role": "system", "content": "あなたは優秀な日本語のエディターです。"},
+                        {"role": "user", "content": prompt}
+                    ]
+                    text_modified = openai.ChatCompletion.create(
+                        model="gpt-3.5-turbo",
+                        messages=messages,
+                        temperature=0,
+                    )
+                    texts_modified=texts_modified+text_modified["choices"][0]["message"]["content"]
+            st.success('***日本語の修正を完了しました***')
+            st.write("***文字起こし結果(修正後)***")
+            st.write(texts_modified)
