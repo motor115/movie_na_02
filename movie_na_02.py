894b5508ebcd2a4a4eae9684d1822459ca0630bb
motor115 <motor115@gmail.com>
2024-02-19 01:19:23 +0900

    Update movie_na_02.py

--- movie_na_02.py
+++ movie_na_02.py
@@ -23,8 +23,7 @@ st.title('音声文字起こしアプリ')
 upload_file = st.file_uploader('音声文字起こしするファイルを選択してください  \nAPIの上限により25MB以上のファイルは文字起こし不可です。\
                                 ファイルを分割する等容量を少なくしてください', type=['m4a', 'mp3', 'webm', 'mp4', 'mpga', 'wav'])
 
-def transcribe():
-    if upload_file is not None:
+   if upload_file is not None:
         st.subheader('ファイル詳細')
         file_details = {'FileName': upload_file.name, 'FileType': upload_file.type, 'FileSize': upload_file.size}
         st.write(file_details)
